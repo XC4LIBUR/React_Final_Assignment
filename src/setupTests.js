62caa6c60ecbd1861ac7d818f65d5c4eb92520f7
XC4LIBUR <deljojose007@gmail.com>
2023-03-19 23:18:07 +0530

    modified:   src/setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react";
+import ReservationForm from "./components/sections/reservePages/BookingForm";
+
+test('Renders the BookingForm heading', () => {
+    render(<ReservationForm />);
+    const headingElement = screen.getByText("Reserve");
+    expect(headingElement).toBeInTheDocument();
+})
